f053b26e6562e648b9b0fe991ad573cd02f26993
Blake Mitchell <mblakeandrew@gmail.com>
2019-06-25 12:10:19 -0700

    created more routes and files

--- hercules/src/App.js
+++ hercules/src/App.js
@@ -2,20 +2,63 @@ import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
 //import PrivateRoute from './PrivateRoute'
-import Login from './components/auth/login'
-import {BrowserRouter as Router,Link,Route} from 'react-router-dom'
-import Signup from './components/auth/signup'
+import Login from './components/auth/Login'
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import Signup from './components/auth/Signup'
 import Home from "./components/Home";
 import Nav from "./components/Nav";
+import Profile from "./components/Profile";
+//import AddExcercise from "./components/AddExcercise";
+import SelectExcercises from "./components/SelectExcercises";
+
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Nav />
-        <Home />
+        <Route
+          exact path="/"
+          render={() => (
+            <Home />
+          )}
+        />
+
+        <Route
+          exact path="/profile"
+          render={() => (
+            <Profile />
+          )}
+        />
+
+        
+
+        <Route
+          exact path="/login"
+          render={() => (
+            <Login />
+          )}
+        />
+
+        <Route
+          exact path="/select-excercises"
+          render={() => (
+            <SelectExcercises />
+          )}
+        />
+
       </div>
     </Router>
-)}
+  )
+}
+
+/*
+<Route
+          exact path="/add-excercise"
+          render={() => (
+            <AddExcercise />
+          )}
+        />
+        */
 
 export default App;
