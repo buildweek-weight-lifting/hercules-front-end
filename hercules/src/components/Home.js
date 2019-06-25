827283239321fd60cdeede7d07af7c7baf2cc6a3
Blake Mitchell <mblakeandrew@gmail.com>
2019-06-24 21:05:08 -0700

    improved styling on home page, got drop down menu on nav bar working

--- hercules/src/components/Home.js
+++ hercules/src/components/Home.js
@@ -4,6 +4,7 @@ const Home = props => {
 
   return(
     <div className="Home">
+      <img src="./images/hercules-logo.svg" />
       <h1>Hercules</h1> 
 
       <button>left</button>
--- hercules/src/components/Nav.js
+++ hercules/src/components/Nav.js
@@ -3,15 +3,41 @@ import { NavLink } from 'react-router-dom';
 import "./components.css"
 
 
-const Nav = props => {
+class Nav extends React.Component{
+  state = {
+    dropped: false
+  }
 
-  return(
-    <div className="Nav">
-      <NavLink>menu</NavLink>
-      <NavLink>logo</NavLink>
-      <NavLink>profile</NavLink>
-    </div>
-  );
+  dropMenu = () => {
+    this.setState({dropped: !this.state.dropped});
+    //console.log(this.state.dropped);
+  }
+
+  render(){
+    return(
+      <div className="Nav">
+  
+        <div className="mobile-bar">
+          <div onClick={this.dropMenu}> menu </div>
+          <div>logo </div>
+          <NavLink exact to="/">profile</NavLink>
+        </div>
+        
+        
+          <div className={  this.state.dropped ? "drop-down" : "up"}>
+            <NavLink exact to="/">Home</NavLink>
+            <NavLink exact to="/">Add Excercise</NavLink>
+            <NavLink exact to="/">Sign Out</NavLink>
+          </div>
+       
+
+        
+      </div>
+      
+    );
+
+  }
+  
 }
 
 export default Nav;
